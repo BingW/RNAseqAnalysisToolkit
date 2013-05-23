b726c9f9351a48573b63f1897fbd8465492f0d00
Bing Wang <toaya.kase@gmail.com>
2013-05-23 12:10:38 -0500

    add time left to Count reads by genes

--- Counting_Reads_by_Genes.py
+++ Counting_Reads_by_Genes.py
@@ -1,9 +1,18 @@
 #! /usr/bin/env python
 #coding: utf-8
 
+'''
+version 0.01
+basic function
+version 0.02
+add time left
+'''
+
 import argparse
 import HTSeq
 import itertools
+import time
+import os,sys
 
 parser = argparse.ArgumentParser(description="Use HTSeq count reads by genes",\
         usage='./%(prog)s sam_file gtf_file > out_file')
@@ -12,6 +21,7 @@ parser.add_argument('gtf_file',help='input gtffile')
 
 args = parser.parse_args()
 
+sys.stderr.write("Processing..\n")
 gtf_file = HTSeq.GFF_Reader(args.gtf_file)
 exons = HTSeq.GenomicArrayOfSets("auto")
 for feature in gtf_file:
@@ -23,8 +33,16 @@ for feature in gtf_file:
     if feature.type == "exon":
         counts[feature.name] = 0
 
+cmd = "wc -l %s"%(args.sam_file)
+count_line = os.popen(cmd).readline()
+os.system("purge")
+total_count = float(count_line.strip().split(" ")[0])
+percent = 0.0
+
+sys.stderr.write("Start Counting:\n")
 sam_file = HTSeq.SAM_Reader(args.sam_file)
-for alnmt in sam_file:
+start_time = time.time()
+for i,alnmt in enumerate(sam_file):
     if alnmt.aligned:
         intersection_set = None
         for iv2, step_set in exons[alnmt.iv].steps():
@@ -34,6 +52,14 @@ for alnmt in sam_file:
                 intersection_set.intersection_update(step_set)
         if len(intersection_set) == 1:
             counts[list(intersection_set)[0]] += 1
+    if i/total_count > percent:
+        time_used = time.time() - start_time
+        time_left = int(time_used/(i/total_count) - time_used)
+        sys.stderr.write("%.2f %% finished\n"%(i/total_count*100))
+        sys.stderr.write("%d h %d m %d s left\n"%(int(time_left/3600),\
+                int((time_left%3600)/60),(time_left%3600)%60))
+        percent += 0.05
 
 for name in sorted(counts.keys()):
-    print name,counts[name]
+    pass
+    #print name,counts[name]
